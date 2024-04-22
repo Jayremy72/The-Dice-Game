28f4b3a39266f31f4bc1d5463f8dd7c77c6d037c
Jeremy <158453758+Jayremy72@users.noreply.github.com>
2024-04-22 13:32:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var imageSelector1 = document.querySelector(".img1");
+var randomNumber1 = Math.floor (Math.random() * 6) + 1;
+
+var imageSelector2 = document.querySelector(".img2");
+var randomNumber2 = Math.floor (Math.random() * 6) + 1;
+ 
+
+var img1Name = "dice" + randomNumber1 + ".png";
+var img2Name = "dice" + randomNumber2 + ".png";
+
+
+var imageSource1 = "images/" + img1Name
+var imageSource2 = "images/" + img2Name
+
+  imageSelector1.setAttribute("src", imageSource1);
+  console.log(randomNumber1);
+
+  imageSelector2.setAttribute("src", imageSource2);
+  console.log(randomNumber2);
+
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "Player 1 Wins!!";
+  }
+  else if (randomNumber1 === randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "Draw";
+  }
+  else {document.querySelector(".container h1").innerHTML = "Player 2 Wins!!"; 
+       }   
